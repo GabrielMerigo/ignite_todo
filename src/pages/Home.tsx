38d7f55fb447a597aa4b526432699c7b3c960873
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-06-30 09:55:37 -0300

    adicioando awesome alert

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
+
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import AwesomeAlert from 'react-native-awesome-alerts';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
@@ -7,34 +9,30 @@ import { TodoInput } from '../components/TodoInput';
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [showAlert, setShowAlert] = useState(false)
 
   function handleAddTask(newTaskTitle: string) {
-      setTasks((state) => [...state, {
-        id: new Date().getTime(),
-        title: newTaskTitle,
-        done: false
-      }])
+    if (tasks.find(task => task.title === newTaskTitle)) return setShowAlert(true)
+
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    const newTasksList = tasks.map((item) => {
-      if (item.id === id) {
-        return {
-          ...item,
-          done: !item.done
-        }
-      } else {
-        return item;
-      }
-    });
-
-    setTasks(newTasksList);
+    const tasksUpdated = tasks.map(task => ({ ...task }))
+    const currentTask = tasksUpdated.find(task => task.id === id)
+    currentTask!.done = !currentTask!.done
+    setTasks(tasksUpdated)
   }
 
   function handleRemoveTask(id: number) {
-    const newFilteredTaskList = tasks.filter((item) => item.id !== id);
-
-    setTasks(newFilteredTaskList);
+    const tasksFiltered = tasks.filter(task => task.id !== id);
+    setTasks(tasksFiltered)
   }
 
   return (
@@ -42,12 +40,24 @@ export function Home() {
       <Header tasksCounter={tasks.length} />
 
       <TodoInput addTask={handleAddTask} />
-      
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
+      />
+
+      <AwesomeAlert
+        show={showAlert}
+        showProgress={false}
+        title="Atenção!"
+        message="Você não pode adicionar tasks com o mesmo nome"
+        closeOnTouchOutside={true}
+        closeOnHardwareBackPress={false}
+        showCancelButton={true}
+        cancelText="OK"
+        cancelButtonColor='#8257E5'
+        onCancelPressed={() => { setShowAlert(false) }}
       />
     </View>
   )
